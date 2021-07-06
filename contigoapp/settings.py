b463abeeda7324887d7c28c07b1e2f59a1aeac37
sheche24 <wlsrmssla@naver.com>
2021-07-06 15:54:17 +0900

    django contigo app

--- contigoapp/settings.py
+++ contigoapp/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]   # "*" 모든 경로에서 접속 가능
 
 
 # Application definition

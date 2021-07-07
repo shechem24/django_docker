c068ed3aca9229ba1461bc46fbf8366c763124fe
sheche24 <wlsrmssla@naver.com>
2021-07-07 20:14:24 +0900

    django test buf

--- contigoapp/settings.py
+++ contigoapp/settings.py
@@ -15,7 +15,41 @@ import os, environ
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+
+
+ALLOWED_HOSTS = ["*"]   # "*" 모든 경로에서 접속 가능
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Application definition
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contigoapp.settings.local')    # settings 경로 다시 지정
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contigoapp.settings')    # settings 경로 다시 지정
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

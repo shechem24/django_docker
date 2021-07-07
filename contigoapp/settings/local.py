3295d4da336972daefc43a978a0327b9f5019ad9
sheche24 <wlsrmssla@naver.com>
2021-07-07 21:44:01 +0900

    django mariadb

---
+++ contigoapp/settings/local.py
@@ -0,0 +1,33 @@
+from .base import *
+
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = ["*"]   # "*" 모든 경로에서 접속 가능
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+

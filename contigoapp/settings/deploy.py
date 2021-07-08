45d10d458c674adcc1ca05a5eb1a5bd6ce5495e0
sheche24 <wlsrmssla@naver.com>
2021-07-08 12:41:55 +0900

    django docker secret

--- contigoapp/settings/deploy.py
+++ contigoapp/settings/deploy.py
@@ -1,5 +1,13 @@
 from .base import *
 
+# docker secret 변수 불러오기
+def read_secret(secretName):
+    file = open('/run/secrets/' + secretName)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()  # 좌우에 불필요한 공백 제거
+    file.close()
+    return secret
+
 
 environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
@@ -14,7 +22,8 @@ env = environ.Env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = ["*"]   # "*" 모든 경로에서 접속 가능
 
@@ -30,8 +39,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         # 'USER': 'django',
-        'PASSWORD': 'password1234',
-        'HOST': 'mariadb',                  # 컨테이너에서 생성한 mariadb 이름
+        # 'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_ROOT_PASSWORD'), # docker secret에 저장한 변수 정보
+        'HOST': 'mariadb',                              # 컨테이너에서 생성한 mariadb 이름
         'PORT': '3306',
     }
 }
